7c5067ece3ba5ba92bb65ea19356b64f58112b81
ilkINDE <luca.mosconi@studenti.unicam.it>
2026-04-13 15:42:09 +0200

    modifica MonsterFactory e Monster, migliorato lo scaling della difficoltà

--- src/main/java/it/unicam/cs/mpgc/rpg123388/model/villain/Monster.java
+++ src/main/java/it/unicam/cs/mpgc/rpg123388/model/villain/Monster.java
@@ -3,7 +3,7 @@ package it.unicam.cs.mpgc.rpg123388.model.villain;
 import it.unicam.cs.mpgc.rpg123388.model.BaseCharacter;
 
 public abstract class Monster extends BaseCharacter {
-    private final int xpReward;
+    private int xpReward;
 
     public Monster(String name, int maxHealth, int attackPower, int xpReward) {
         super(name, maxHealth, attackPower);
@@ -12,6 +12,18 @@ public abstract class Monster extends BaseCharacter {
 
     public int getXpReward() {
         return xpReward;
+    }
+
+    public void scaleDifficulty(int roomLevel) {
+        int healthBoost = roomLevel * 3;
+
+        int attackBoost = roomLevel / 2;
+
+        int xpBoost = roomLevel * 2;
+
+        this.increaseMaxHealth(healthBoost);
+        this.increaseAttackPower(attackBoost);
+        this.xpReward += xpBoost;
     }
 
 }
--- src/main/java/it/unicam/cs/mpgc/rpg123388/model/villain/MonsterFactory.java
+++ src/main/java/it/unicam/cs/mpgc/rpg123388/model/villain/MonsterFactory.java
@@ -7,34 +7,35 @@ import java.util.Random;
 public class MonsterFactory {
 
     private final Random random = new Random();
-    private int roomCounter = 0; // Per tenere traccia delle stanze completate
+    private int roomCounter = 0;
 
     public List<Monster> generateNextRoom() {
         roomCounter++;
         List<Monster> encounter = new ArrayList<>();
 
-        // Ogni 5 stanze spawna il Boss
         if (roomCounter % 5 == 0) {
             encounter.add(new Dragon());
-            // Magari con un paio di sciamani come scorta
             encounter.add(new GoblinShaman());
             encounter.add(new GoblinShaman());
-            return encounter;
+        } else {
+            int numMonsters = random.nextInt(3) + 1;
+
+            for (int i = 0; i < numMonsters; i++) {
+                int roll = random.nextInt(100);
+
+                if (roomCounter > 2 && roll < 30) {
+                    encounter.add(new Orc());
+                } else if (roll < 60) {
+                    encounter.add(new GoblinShaman());
+                } else {
+                    encounter.add(new Goblin());
+                }
+            }
         }
 
-        int numMonsters = random.nextInt(3) + 1;
-
-        for (int i = 0; i < numMonsters; i++) {
-            int roll = random.nextInt(100);
-
-            // Più si va avanti, più è probabile trovare Orchi forti
-            if (roomCounter > 2 && roll < 30) {
-                encounter.add(new Orc());
-            } else if (roll < 60) {
-                encounter.add(new GoblinShaman());
-            } else {
-                encounter.add(new Goblin());
-            }
+        // In base alla stanza in cui si trova, il mostro viene potenziato
+        for (Monster m : encounter) {
+            m.scaleDifficulty(roomCounter);
         }
 
         return encounter;
